a721033571fc9430ef3830821e34b114417f32d3
Melissa <mystic101@gmail.com>
2020-11-11 16:59:57 +0000

    POST/redirect/GET pattern

--- app.rb
+++ app.rb
@@ -1,16 +1,25 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @name1 = params[:name1]
-    @name2 = params[:name2]
+    session[:name1] = params[:name1]
+    session[:name2] = params[:name2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name1 = session[:name1]
+    @name2 = session[:name2]
     erb :play
   end
+
+
 
   run! if app_file == $0
 
