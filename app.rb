0dd392ae8cf3a0db432b6a9ea1cd5efccfadd2ff
Melissa <mystic101@gmail.com>
2020-11-12 19:03:28 +0000

    Extracting Logic to the Model

--- app.rb
+++ app.rb
@@ -8,20 +8,20 @@ enable :sessions
   end
 
   post '/names' do
-    session[:name1] = params[:name1]
-    session[:name2] = params[:name2]
+    $player_1 = Player.new(params[:name1])
+    $player_2 = Player.new(params[:name2])
     redirect '/play'
   end
 
   get '/play' do
-    @name1 = session[:name1]
-    @name2 = session[:name2]
+    @name1 = $player_1.name
+    @name2 = $player_2.name
     erb :play
   end
 
   get '/attack' do
-    @name1 = session[:name1]
-    @name2 = session[:name2]
+    @name1 = $player_1.name
+    @name2 = $player_2.name
     erb :attack
   end
 
